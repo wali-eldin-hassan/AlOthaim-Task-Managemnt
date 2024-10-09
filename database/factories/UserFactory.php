1b9ee1ae4cfa8388d585edbbaf72d65722f19b20
wali-eldin-hassan <creaspo6@gmail.com>
2024-10-09 14:11:30 +0300

    added nofcations and login and register test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,7 +32,7 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(
-            fn(array $attributes) => [
+            fn (array $attributes) => [
                 'email_verified_at' => null,
             ]
         );
---
+++ tests/Feature/NotifcationsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Task;
+use App\Models\User;
+use App\Notifications\TaskAssignedNotification;
+
+it('can get his notifications', function () {
+    $user = create(User::class);
+
+    $task = create(Task::class);
+
+    $task->user->notify(new TaskAssignedNotification($task));
+
+    $this->actingAs($user)
+        ->get('/')
+        ->assertOk()
+        ->assertViewHas('notifications');
+
+});
+
+it('can mark his notifications as read', function () {
+    $user = create(User::class);
+
+    $task = create(Task::class, ['assigned_to' => $user->id]);
+
+    $task->user->notify(new TaskAssignedNotification($task));
+
+    $this->actingAs($user)
+        ->post(route('notifications.markAllAsRead'))
+        ->assertRedirect();
+
+    $user->refresh();
+    foreach ($user->unreadNotifications as $notification) {
+        $this->assertNotNull($notification->read_at);
+    }
+});
