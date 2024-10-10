dfeaa3b1bf91102697d651e05990af1baeedfda2
wali-eldin-hassan <creaspo6@gmail.com>
2024-10-10 08:07:11 +0300

    fix talwaind nested css isue

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -57,7 +57,7 @@
 
     </x-slot>
 
-    <section class="px-4 mx-auto">
+    <section class="container px-4 mx-auto">
 
         <div class="flex flex-col">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
